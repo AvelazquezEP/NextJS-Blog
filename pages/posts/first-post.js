1a005315dae5e586b0a075c98cec8edf3bc3810d
Alberto Alejandro Martinez Velazquez <avelazquez@abogadoericprice.com>
2023-10-23 14:05:58 -0700

    FIX: the way to can make the blog, and how to show all the data with different functions/variables and import/export

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,16 +1,15 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Script from 'next/script';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
     // return <h1>First Post</h1>;
     return (
-        <>
-            {/* Metadata */}
+        <Layout>
             <Head>
                 <title>First Post</title>
                 {/* <script src="https://connect.facebook.net/en_US/sdk.js"></script> */}
-                
             </Head>
 
             <Script
@@ -25,7 +24,7 @@ export default function FirstPost() {
             <h2>
                 <Link href="/">Back to home</Link>
             </h2>
-        </>
-    );
+        </Layout>
+    )
 }
 
