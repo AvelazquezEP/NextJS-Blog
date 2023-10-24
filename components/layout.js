cf9815b7e114ee885adca8324d37b5f9aab19522
Alberto Alejandro Martinez Velazquez <avelazquez@abogadoericprice.com>
2023-10-23 17:08:38 -0700

    UPDATE: install some package for try to read the .md files and get some variables

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Test name'
+const name = 'Test name';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function layout({ children, home }) {
